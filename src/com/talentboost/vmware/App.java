a3d573f0920eb0f7804834791d009d7375e38579
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2015-08-18 23:09:49 +0300

    more examples

--- src/com/talentboost/vmware/App.java
+++ src/com/talentboost/vmware/App.java
@@ -8,14 +8,16 @@ public class App {
 
 		DTreeNew treeWords = new DTreeNew();
 		FillDTree.fill("filteredWords", treeWords);
-		String phrase = "4264663";
+		//String phrase = "43335968"; 
+		String phrase = "4262665"; //i-am-cool
+		//String phrase = "45453968";
 		//String phrase = "433354663";
 		String word = "966337385";
 		List<String> phrases = treeWords.numberToPhrase(phrase);
 		List<String> words = treeWords.numberToWord(word);
 		
-		for (String wordPhone : phrases) {
-			System.out.println(wordPhone);
+		for (String phrasePhone : phrases) {
+			System.out.println(phrasePhone);
 		}
 		System.out.println(words.size());
 	}
